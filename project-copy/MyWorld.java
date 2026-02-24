7e28e779c9857fa944f8967a29fba88422f30415
chloefoulston <chloefoulston06@gmail.com>
2026-02-24 13:36:23 -0500

    Add files via upload

---
+++ project-copy/MyWorld.java
@@ -0,0 +1,30 @@
+// WARNING: This file is auto-generated and any changes to it will be overwritten
+import lang.stride.*;
+import java.util.*;
+import greenfoot.*;
+
+/**
+ * 
+ */
+public class MyWorld extends World
+{
+
+    /**
+     * Constructor for objects of class MyWorld.
+     */
+    public MyWorld()
+    {
+        super(600, 400, 1);
+        prepare();
+    }
+
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        Bee bee =  new Bee();
+        addObject(bee, 289, 199);
+    }
+}
