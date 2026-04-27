61ffe29bb82adc2fab4259fb1eb1d39dafacfaf3
Shane2203 <rushanechryzl@gmail.com>
2026-04-27 15:11:06 -0400

    dadada

--- HealthBar.java
+++ HealthBar.java
@@ -11,14 +11,14 @@ public class HealthBar extends Player
     protected int health = 4;
     protected int healthBarWidth = 80;
     protected int healthBarHeight = 15;
-    protected int pixelsPerHealthPoint = ( int ) healthBarWidth / health;
+    protected int pixelsPerHealthPoint = (int)healthBarWidth / health;
 
     /**
      * Act - do whatever the HealthBar wants to do. This method is called whenever the 'Act' or 'Run' button gets pressed in the environment.
      */
     public HealthBar()
     {
-        update ( );
+        update();
     }
 
     /**
@@ -26,7 +26,7 @@ public class HealthBar extends Player
      */
     public void act()
     {
-        update ( );
+        update();
     }
 
     /**
@@ -34,12 +34,12 @@ public class HealthBar extends Player
      */
     public void update()
     {
-        setImage ( new GreenfootImage ( healthBarWidth + 2 , healthBarHeight + 2 ) );
-        GreenfootImage myImage = getImage ( );
-        myImage . setColor ( Color . WHITE );
-        myImage . drawRect ( 0 , 0 , healthBarWidth + 1 , healthBarHeight + 1 );
-        myImage . setColor ( Color . RED );
-        myImage . fillRect ( 1 , 1 , health * pixelsPerHealthPoint , healthBarHeight );
+        setImage( new  GreenfootImage(healthBarWidth + 2, healthBarHeight + 2));
+        GreenfootImage myImage = getImage();
+        myImage.setColor(Color.WHITE);
+        myImage.drawRect(0, 0, healthBarWidth + 1, healthBarHeight + 1);
+        myImage.setColor(Color.RED);
+        myImage.fillRect(1, 1, health * pixelsPerHealthPoint, healthBarHeight);
     }
 
     /**
