68af53e8f3aa01474e16a33392ced5faaf7583f2
chloefoulston <%chloefoulston06@gmail.com%>
2026-05-10 00:49:15 -0400

    added the ability to play again in the endScreen

--- endGameWorld.java
+++ endGameWorld.java
@@ -8,24 +8,34 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class endGameWorld extends World
 {
-
     /**
      * Constructor for objects of class endGameWorld.
      * 
      */
     public endGameWorld(String winner)
     {    
-        super(600, 400, 1); 
+        super(600, 400, 1);
+        
         if (winner.equals("P1"))
         {
             setBackground("devWin.png");
             showText("PLAYER 1 WINS!", 295, 355);
+            showText("press space to play again", 295, 385);
         }
         else if (winner.equals("P2"))
         {
             setBackground("wizWin.png");
             showText("PLAYER 2 WINS!", 295, 355);
+            showText("press space to play again", 295, 385);
         }
+        
     }
     
+    public void act()
+    {
+        if (Greenfoot.isKeyDown("space"))
+        {
+            Greenfoot.setWorld(new CoreArena());
+        }
+    }
 }
