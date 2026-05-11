b4a86883ddf5b2757a9e9214b97c80058147d92e
chloefoulston <%chloefoulston06@gmail.com%>
2026-05-10 20:13:51 -0400

    all done i think?!?

--- Player.java
+++ Player.java
@@ -17,24 +17,18 @@ public abstract class Player extends Actor
     protected GreenfootImage specialRIGHT = null;
     protected String facing;
     protected String winner;
-    
     protected static final int MAX_HEALTH = 100;
     protected int health = MAX_HEALTH;
-    
     protected final int GRAVITY = 1;
     protected int vSpeed = 4;
     protected int jumpPower = -15;
-    protected int ground = 270;   
-    
+    protected int ground = 270;
     protected int coolDown = 0;
     protected HealthBar healthBar;
     protected int endTimer = -1;
     protected static int wins;
     protected boolean canUseSpecial = false;
-    /**
-     * Act - do whatever the Player wants to do. This method is called whenever the 'Act' or 'Run' button gets pressed in the environment.
-     */
-    
+
     public void act()
     {
         move();
@@ -43,21 +37,23 @@ public abstract class Player extends Actor
         attack();
         specialAttack();
         reduceCooldown();
-        die();
-         if (endTimer > 0){
-            endTimer--;
-            if (endTimer == 0){
-            Greenfoot.setWorld(new endGameWorld(winner));
+        if (endTimer > 0) {
+            endTimer = endTimer - 1;
+            if (endTimer == 0) {
+                Greenfoot.setWorld( new  endGameWorld(winner));
             }
             return;
         }
     }
-    public abstract void move();
     
-    public abstract void jump();
-    
-    public abstract void attack();
-    public abstract void specialAttack();
+    abstract void move();
+
+    abstract void jump();
+
+    abstract void attack();
+
+    abstract void specialAttack();
+
     /**
      * getDirection - returns the direction the player is facing
      */
@@ -65,63 +61,61 @@ public abstract class Player extends Actor
     {
         return facing;
     }
-    
+
     /**
      * applyGravity - applies gravity to the player to allow jumping
      */
     public void applyGravity()
     {
         setLocation(getX(), getY() + vSpeed);
-        vSpeed += GRAVITY;
-
-        if (getY() >= ground)
-        {
+        vSpeed = vSpeed + GRAVITY;
+        if (getY() >= ground) {
             setLocation(getX(), ground);
             vSpeed = 0;
         }
     }
-    
-    public boolean isOnGround(){
+
+    public boolean isOnGround()
+    {
         return getY() >= ground;
     }
-    
+
     public int getHealth()
     {
         return health;
     }
-    
+
     public void reduceCooldown()
     {
-        if (coolDown > 0)
-        {
-            coolDown--;
+        if (coolDown > 0) {
+            coolDown = coolDown - 1;
         }
     }
-    
+
     public void setHealthBar(HealthBar bar)
     {
         healthBar = bar;
         healthBar.updateBar();
     }
-    
+
     public void takeDamage(int amount)
     {
-        if (!isOnGround()){
+        if ( ! isOnGround()) {
             return;
         }
-        health -= amount;
-        if (health < 0){
+        health = health - amount;
+        if (health < 0) {
             health = 0;
         }
-        if (healthBar != null){
+        if (healthBar != null) {
             healthBar.updateBar();
         }
-        
-         if (health == 0){
-            if (this instanceof P1){
-            winner = "P2"; 
-            }else if (this instanceof P2){
-            winner = "P1";
+        if (health == 0) {
+            if (this instanceof P1) {
+                winner = "P2";
+            }
+            else if (this instanceof P2) {
+                winner = "P1";
             }
             deathImage();
             getWorld().showText(getClass().getName() + " loses!", 300, 100);
@@ -129,75 +123,72 @@ public abstract class Player extends Actor
             endTimer = 180;
         }
     }
-    
-    public void die()
+  
+    public void canUseSpecial()
     {
-        if(health == 0){
-            deathImage();
+        if (health <= 30) {
+            canUseSpecial = true;
+        }
+        else {
+            canUseSpecial = false;
         }
     }
-    
-    public void walkingImage()
+   
+    public void heal(int amount)
     {
-        if(getDirection().equals("left")){
-            setImage(leftImage);
+        health = health + amount;
+        if (health > MAX_HEALTH) {
+            health = MAX_HEALTH;
         }
-        if(getDirection().equals("right")){
-            setImage(rightImage);
-        }        
-    }
-    
-    public void fightingImage()
-    {
-        if(getDirection().equals("left")){
-            setImage(attackLeft);
-        }
-        if(getDirection().equals("right")){
-            setImage(attackRight);
+        if (healthBar != null) {
+            healthBar.updateBar();
         }
     }
-    
-    public void deathImage()
-    {
-        if(getDirection().equals("left")){
-            setImage(dieLeft);
-        }
-        if(getDirection().equals("right")){
-            setImage(dieRight);
-        }      
-    }
-    public void specialAtkImage()
-    {
-        if(getDirection().equals("left")){
-            setImage(specialLEFT);
-        }
-        if(getDirection().equals("right")){
-            setImage(specialRIGHT);
-        }      
-        
-    }
+  
     public void resetWins()
     {
         wins = 0;
     }
-    public void canUseSpecial()
+
+    public void walkingImage()
     {
-        if (health <= 30){
-            canUseSpecial = true;
-        }else{
-            canUseSpecial = false;
+        if (getDirection().equals("left")) {
+            setImage(leftImage);
+        }
+        if (getDirection().equals("right")) {
+            setImage(rightImage);
         }
     }
-    public void heal(int amount)
+
+    public void fightingImage()
     {
-        health += amount;
-        if (health > MAX_HEALTH)
-        {
-            health = MAX_HEALTH;
+        if (getDirection().equals("left")) {
+            setImage(attackLeft);
         }
-        if (healthBar != null)
-        {
-            healthBar.updateBar();
+        if (getDirection().equals("right")) {
+            setImage(attackRight);
         }
     }
+
+    public void deathImage()
+    {
+        if (getDirection().equals("left")) {
+            setImage(dieLeft);
+        }
+        if (getDirection().equals("right")) {
+            setImage(dieRight);
+        }
+    }
+
+    public void specialAtkImage()
+    {
+        if (getDirection().equals("left")) {
+            setImage(specialLEFT);
+        }
+        if (getDirection().equals("right")) {
+            setImage(specialRIGHT);
+        }
+    }
+
+    
 }
